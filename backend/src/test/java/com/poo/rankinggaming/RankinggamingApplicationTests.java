f5fcc80e76e5bf59724ecffc9a966f04837cedec
LaurianoMonte <laurianomonte@gmail.com>
2022-05-02 12:12:40 -0300

    Projeto criado

---
+++ backend/src/test/java/com/poo/rankinggaming/RankinggamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poo.rankinggaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankinggamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
